dce368eb32e9dedb090f99407749944a055f11e3
Madhvik007 <madhvikkathiria@gmail.com>
2025-11-27 20:28:24 +0530

    Added Text Selection Color for btter UX

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "NEO_BRUTAL_PORTFOLIO",
+  title: "MY_PORTFOLIO",
   description: "A raw, unfiltered showcase of digital craftsmanship.",
 };
 
